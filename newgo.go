cd39d15356408f312f86de0fa2059fd39b58d583
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-02 10:32:23 +0530

    var

--- newgo.go
+++ newgo.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main(){
 	var a  int32 = 10
-	b := 20
+	b := 20.222
 	fmt.Println("This is address : ",&a,&b)
 	fmt.Println("This is Value : ",a,b)
+	stringMain()
+}
+
+func stringMain(){
+	var Name string = "Amy Stanly"
+	fmt.Println("Name : ",Name)
+	fmt.Println("Length of the string : ",len(Name))
 }
