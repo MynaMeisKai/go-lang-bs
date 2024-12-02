c23056b9ba56ef96e466703eddd8f2ed82e05a5f
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-02 20:36:52 +0530

    for loop

--- newgo.go
+++ newgo.go
@@ -2,17 +2,52 @@ package main
 
 import "fmt"
 
-// formate print function
+// for loop
 
 func main()	{
-	
-	a:= "Amy Stanly"
-	b:= 221
-	c:= 4.214653
 
-	fmt.Printf("%T\n",a) // Type of Data is Stored
-	fmt.Printf("%d %T \n",b,b) // Value and the  Type
-	fmt.Printf("%.3f %T \n",c,c) // Value with given no of floating points  and the  Type
-	fmt.Printf("%b \n",8) // 32 16 8 4 2 1 binary value 
-	fmt.Printf("%x",42) // hex value 
+	// a := "Sample Foright "
+
+	// for i := 0 ; i< len(a) ;i++ {
+	// 	fmt.Println(string(a[i]))
+	// }
+	// for _,char := range a {
+	// 	fmt.Println(string(char))
+	// }
+	// loop1()
+	// loop2()
+	loop3()
+	loop4()
+}
+
+func loop1(){
+	for i:=1 ; i<=10 ; i ++{
+		fmt.Println(i)
+	}
+	
+}
+func loop2(){
+	i:=1
+	for i<=10{
+		fmt.Println(i)
+		i++
+	}
+	
+}
+func loop3(){
+	for i:=1 ; i<5 ; i++{
+		for j:=0 ; j<i ; j++{
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
+}
+
+func loop4(){
+	for i:=5 ; i>=0 ; i--{
+		for j:=0 ; j<i ; j++{
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
 }
