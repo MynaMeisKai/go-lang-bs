630f53be8ee937bd66ba7e62a070c614ba9edcd5
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-02 20:07:23 +0530

    printf

--- newgo.go
+++ newgo.go
@@ -2,14 +2,17 @@ package main
 
 import "fmt"
 
+// formate print function
+
 func main()	{
+	
+	a:= "Amy Stanly"
+	b:= 221
+	c:= 4.214653
 
-	x := 10
-	fmt.Println("Old Value :>>> ",x)
-	changeValue(&x)
-	fmt.Println("New Value :>>> ",x)
-}
-
-func changeValue(x *int)	{
-	*x = 20
+	fmt.Printf("%T\n",a) // Type of Data is Stored
+	fmt.Printf("%d %T \n",b,b) // Value and the  Type
+	fmt.Printf("%.3f %T \n",c,c) // Value with given no of floating points  and the  Type
+	fmt.Printf("%b \n",8) // 32 16 8 4 2 1 binary value 
+	fmt.Printf("%x",42) // hex value 
 }
