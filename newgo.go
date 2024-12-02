95b58e4251d82962755c6b30bb72c0437e928a8e
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-02 10:18:07 +0530

    var

--- newgo.go
+++ newgo.go
@@ -1,9 +1,10 @@
 package main
+
 import "fmt"
 
 func main(){
-	// fmt.Println("Hello world~")
 	var a  int32 = 10
 	b := 20
-	fmt.Println(a,b)
+	fmt.Println("This is address : ",&a,&b)
+	fmt.Println("This is Value : ",a,b)
 }
