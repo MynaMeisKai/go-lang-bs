20b951e0c1602a20a8774b8d08c8b0a76e5f5ada
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-03 10:18:08 +0530

    Array

--- newgo.go
+++ newgo.go
@@ -5,10 +5,23 @@ import "fmt"
 // Array
 
 func main()	{
-	Anum := [5]int{1,2,3,4,5}
-	// fmt.Println(Anum)
+	Anum := [5]int{2,4,6,8,10}
+	fmt.Println(Anum)
 
 	for i:=0 ; i<len(Anum) ; i++ {
 		fmt.Println(Anum[i])
 	}
+
+	// Slice and Reverse
+	slic := Anum[:5]
+	fmt.Println(slic)
+
+	// Copy array 
+	newArray := make([]int,5,10)
+	copy(newArray,slic)
+	fmt.Println(newArray)
+
+	// Append
+	appArray := append(newArray,100,101,103)
+	fmt.Println(appArray)
 }
