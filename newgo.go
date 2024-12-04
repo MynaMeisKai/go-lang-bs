1721b67d2ed22b0f4cb7965642666da06a26bf90
MynaMeisKai <saijanarthanan217@gmail.com>
2024-12-04 18:45:42 +0530

    functions extent

--- newgo.go
+++ newgo.go
@@ -2,38 +2,49 @@ package main
 
 import "fmt"
 
-func sam(){
-	fmt.Println("W-W-W")
+// func to func
+
+func samp1(newFunc func(int)int){
+	fmt.Println(newFunc(9))
+	
+}
+//Defer
+
+func add(x,y int)(z1,z2 int){
+	defer fmt.Println("Good!!")
+	z1 = x+y
+	z2 = x* -y
+	fmt.Println("Before return")
+	return
+
 }
 
-func add(a,b int) int{ // 1 st int is datatyp 2nd int is return value
-	return a+b
-}
+// functoin closure :
 
-func sum1(x,y int) (z1,z2 int){ // z1,z2 are labels
-	return x+y , x*y
+func dos() func()int{
+	count := 0
+	fmt.Println("Do's called !!")
+	return func() int {
+		val := count +1 
+		count = count+1
+		return val
+	}
+
 }
 
 func main(){
 
-	sam() // simple fun call
-	add1 := add       // passing Arguments in func
-	fmt.Println("Sum : ", add1(1,2))
-	
-	// multiple output func 
-	sums,pro:=sum1(7,2)
-	fmt.Println("Ans : ",sums , pro)
-
-	// Anonymous Function 
-	samp1 := func (){
-		fmt.Println("samp-1")
+	test := func (x int)int {
+		return x * -2
 	}
-	samp1()
+	samp1(test) //func --> func
 
-	samp2 := func(x int)int {
-		return x*-2
-	}(2)
-	fmt.Println(samp2)
+	adds,prods := add(2,6)
+	fmt.Printf("Sum : %d \nProduct : %d ",adds,prods) //defer 
 
-
+	funs := dos()
+	for i:=0 ; i<=10 ; i++{
+		fmt.Println(funs())
+	}
 }
+
