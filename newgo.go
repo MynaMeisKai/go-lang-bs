2c32fceec0efba2503f2e2f31ed199c5ecdcfbbc
MynaMeisKai <saijanarthanan217@gmail.com>
2025-01-06 10:49:24 +0530

    Pointer

--- newgo.go
+++ newgo.go
@@ -1,27 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "reflect"
+)
 
-// Immutable's int,float,string,bool,array
+// Pointer
 
-func main(){
+/*
+ * - Astrick or Derefrenc
+ & - GetPointer or Refrence
+*/
 
-    x := 10
-    y := x
-    fmt.Println(x,y)
-    y = 20
-    fmt.Println(x,y)
 
-    var xx[3]int = [3]int {3,4,5}
-    yy := xx
-    fmt.Println(xx)
-    yy[0] = 12
-    fmt.Println(xx,yy)
-
-    a :="Str"
-    b := a
-    b = "samp"
-    fmt.Println(a,b)
+func toChange(sam *string) {
+    *sam = "Bad One"
 }
 
+func main() {
+
+    // x:= 7
+    // y:= &x
+    // fmt.Println(x,y)
+    // *y = 12
+    // fmt.Println(x)
+    
+    // or_str := "Good One"
+    // fmt.Println(or_str)
+
+    // toChange(&or_str)
+    // fmt.Println(or_str)
  
+    to_run := "Duty"
+    var pointer *string = &to_run
+    fmt.Println(*pointer) // Duty
+    fmt.Println(pointer) // O/p Address to to_run
+    fmt.Println(&pointer) // O/p address of pointer
+}
+
+// two way to find TypeOf var or obj
+    // a := 7444
+    // fmt.Printf("%T",a)
+    // fmt.Printf("%v \n",reflect.TypeOf(a))
